0a2cdf40c3173ab2081822b34bfa2a1d52bced43
TomKovac <61820360+TomKovac@users.noreply.github.com>
2023-06-20 16:45:45 +0200

    mend

--- test_L2.ps1
+++ test_L2.ps1
@@ -1,4 +1,4 @@
 # run build
 
-dotnet run --project cake/Build.csproj --do-test true --do-pack false --test-level 2
+dotnet run --project cake/Build.csproj --do-test false --do-pack false
 exit $LASTEXITCODE;
